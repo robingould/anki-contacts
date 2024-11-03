41391d7c69d4f6beda0219fd737e7c48313184c8
Robin Gould <robin.gould@sonic.net>
2024-11-03 15:04:46 -0500

    fix contact model update path in contact service

--- angular-client/src/shared/services/contact.service.ts
+++ angular-client/src/shared/services/contact.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Contact } from '../contact';
+import { Contact } from '../models/contact';
 @Injectable({
   providedIn: 'root'
 })
