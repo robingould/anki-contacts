1945fae3677eb5ba2e0cef51e74419734e1bc266
Robin Gould <robin.gould@sonic.net>
2024-11-03 19:52:54 -0500

    fix api get url, need proper cors

--- angular-client/src/shared/services/contact.service.ts
+++ angular-client/src/shared/services/contact.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ContactService {
-  private apiUrl = "/url"
+  private apiUrl = "http://localhost:8080"
 
   constructor(private http: HttpClient) { }
 
