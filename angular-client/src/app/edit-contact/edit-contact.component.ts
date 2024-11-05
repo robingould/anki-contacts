54fce66dbcd2da5a414e5ee68f06ac95fb32d04e
Robin Gould <robin.gould@sonic.net>
2024-11-05 16:49:27 -0500

    editing contact routing with getting contactid from url done

--- angular-client/src/app/edit-contact/edit-contact.component.ts
+++ angular-client/src/app/edit-contact/edit-contact.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { ContactService } from '../../shared/services/contact.service';
+import { Contact } from '../../shared/models/contact';
 
 @Component({
   selector: 'app-edit-contact',
@@ -7,6 +10,20 @@ import { Component } from '@angular/core';
   templateUrl: './edit-contact.component.html',
   styleUrl: './edit-contact.component.css'
 })
-export class EditContactComponent {
+export class EditContactComponent implements OnInit {
+  public contactID = 0;
 
+  constructor(
+    private contactService: ContactService,
+    private route: ActivatedRoute
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe(async (params) => {
+      this.contactID = params["id"];
+      //this.contact = await this.contactService.getContact(contactID);
+    });
+  }
 }
