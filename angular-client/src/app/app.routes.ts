6b12abeb31b9ce7afce7087a44c42efabdaf2c76
Robin Gould <robin.gould@sonic.net>
2024-11-05 12:58:48 -0500

    Add routes for new component (kinda missleading (because add contact component hadn't been created yet)

--- angular-client/src/app/app.routes.ts
+++ angular-client/src/app/app.routes.ts
@@ -1,4 +1,16 @@
 import { Routes } from "@angular/router";
+import { ContactListComponent } from "./contact-list/contact-list.component";
+import { AddContactComponent } from "./add-contact/add-contact.component";
 
 export const routes: Routes = [
+    {
+        path: "",
+        title: "Contact List Page",
+        component: ContactListComponent,
+    },
+    {
+        path: "add-contact",
+        title: "Add Contact",
+        component: AddContactComponent
+    }
 ];
