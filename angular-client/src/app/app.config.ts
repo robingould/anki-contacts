07f84e9f646478ba2b3cfe9f7d23d0c1eb1103c7
Robin Gould <robin.gould@sonic.net>
2024-11-06 11:16:54 -0500

    add primative http interceptor logging

--- angular-client/src/app/app.config.ts
+++ angular-client/src/app/app.config.ts
@@ -1,9 +1,13 @@
-import { provideHttpClient } from "@angular/common/http";
+import { provideHttpClient, withInterceptors } from "@angular/common/http";
 import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
 import { provideRouter } from "@angular/router";
-
 import { routes } from "./app.routes";
+import { loggerInterceptor } from "../shared/interceptor/logger.interceptor";
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
+	providers: [
+		provideZoneChangeDetection({ eventCoalescing: true }), 
+		provideRouter(routes), 
+		provideHttpClient(withInterceptors([loggerInterceptor]))
+	]
 };
