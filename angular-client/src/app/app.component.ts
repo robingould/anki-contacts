f1c843262125c342c299953d432b18058e7592ea
Robin Gould <robin.gould@sonic.net>
2024-11-07 15:19:37 -0500

    removed unused imports

--- angular-client/src/app/app.component.ts
+++ angular-client/src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from "@angular/core";
-import { FormsModule } from "@angular/forms";
 import { RouterOutlet } from "@angular/router";
-
-import { ContactListComponent } from "./contact-list/contact-list.component";
 
 /**
  * AppComponent is the main component for the app. It basically just serves to
  * contain the rest of it.
  */
 @Component({
-	imports: [RouterOutlet, FormsModule, ContactListComponent],
+	imports: [RouterOutlet],
 	selector: "app-root",
 	standalone: true,
 	styleUrl: "./app.component.css",
