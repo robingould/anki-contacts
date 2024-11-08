f9c8cde5d08947ca357bc08da4024a5757421bd7
Robin Gould <robin.gould@sonic.net>
2024-11-07 19:15:06 -0500

    add testing for sadification pipe

--- angular-client/src/shared/pipes/sad-ify.pipe.spec.ts
+++ angular-client/src/shared/pipes/sad-ify.pipe.spec.ts
@@ -1,8 +1,17 @@
 import { SadIfyPipe } from "./sad-ify.pipe";
 
 describe("SadIfyPipe", () => {
+	const pipe = new SadIfyPipe();
+
 	it("create an instance", () => {
-		const pipe = new SadIfyPipe();
 		expect(pipe).toBeTruthy();
+	});
+
+	it("should turn an empty string into ' :/'", () => {
+		expect(pipe.transform("")).toBe(" :/");
+	});
+
+	it("should turn 'yay I love life!' into 'yay I love life! :/'", () => {
+		expect(pipe.transform("yay I love life!")).toBe("yay I love life! :/");
 	});
 });
