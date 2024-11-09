94b768c1f11353885c6901f25e4cb477de345fb9
Robin Gould <robin.gould@sonic.net>
2024-11-08 21:40:47 -0500

    lint animations import

--- angular-client/src/app/app.config.ts
+++ angular-client/src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { provideHttpClient, withInterceptors } from "@angular/common/http";
 import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 import { provideRouter } from "@angular/router";
 
 import { loggerInterceptor } from "../shared/interceptors/logger.interceptor";
 
 import { routes } from "./app.routes";
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
