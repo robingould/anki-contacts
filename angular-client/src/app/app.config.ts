7068aca3a016e7cc47abad4b3ef30de0f3049f8b
Robin Gould <robin.gould@sonic.net>
2024-11-08 21:32:25 -0500

    adding angular material

--- angular-client/src/app/app.config.ts
+++ angular-client/src/app/app.config.ts
@@ -5,11 +5,12 @@ import { provideRouter } from "@angular/router";
 import { loggerInterceptor } from "../shared/interceptors/logger.interceptor";
 
 import { routes } from "./app.routes";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideZoneChangeDetection({ eventCoalescing: true }),
 		provideRouter(routes),
-		provideHttpClient(withInterceptors([loggerInterceptor]))
+		provideHttpClient(withInterceptors([loggerInterceptor])), provideAnimationsAsync()
 	]
 };
